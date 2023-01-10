7ab1c7021497260f37c96b0b948371f997b00b50
Trohsamuel <trohsamuel@gmail.com>
2023-01-10 18:41:25 -0500

    inheritance

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,14 +1,17 @@
+
 #!/usr/bin/python3
-"""method for student creation"""
+"""defines class Student"""
 
 
-class Student:
-    """Student obj"""
+class Student():
+    """class to define student"""
 
     def __init__(self, first_name, last_name, age):
+        """initializes student instances"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        return self.
+        """returns dictionary descript for JSON serialization"""
+        return self.__dict__
