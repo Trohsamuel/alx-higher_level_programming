73a75778c4de75b9628eddaf6a4bc4643a6d167c
Trohsamuel <trohsamuel@gmail.com>
2023-01-11 18:41:45 -0500

    inheritance

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""script to add all arguments to a Python list and save to a file"""
+"""Defines a class Student."""
 
-from sys import argv
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+class Student:
+    """Represent a student."""
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
 
-try:
-    arglist = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    arglist = []
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-arglist += argv[1:]
-save_to_json_file(arglist, "add_item.json")
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
